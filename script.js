bac5fe2e599514c52d1a723f65f7c6bee4f18772
Carolwanzuu <carolkyalo01@gmail.com>
2021-03-14 19:56:52 +0300

    added a Readme and license

---
+++ script.js
@@ -0,0 +1,16 @@
+var shedA = function(){
+    console.log("510 litres");
+};
+shedA();
+var shedB= function(){
+    console.log("308 litres");
+};
+shedB();
+var shedC = function(){
+    console.log("486 litres");
+};
+shedC();
+var shedD = function(){
+    console.log("572 litres");
+};
+shedD();
